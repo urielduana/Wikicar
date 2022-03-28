56aa5464859064c7017eed23c0739041888e8a7a
Vadose <uri250602@outlook.es>
2022-03-28 14:22:35 -0600

    Casi dos etiquetas

--- pruebaUno.py
+++ pruebaUno.py
@@ -1,5 +1,6 @@
 from totalConnection import *
 dataBaseConnected = totalConnectionDataBase()
-dataBaseConnected.updateUserMail(1, "uriel@gmail.com")
+
+dataBaseConnected.addBrand("Chevrolet", "Pedrito Sanchez y Bumbblebee" , '2022-02-06', "Paises Bajos", "Se encontró un nopal e hizo esta wea")
 
 dataBaseConnected.close()
--- startScreen.py
+++ startScreen.py
@@ -1,17 +1,29 @@
 from tkinter import *
+from tkinter.font import BOLD
 
 class screenLogin(Frame):
     def __init__(self, master):
-        super().__init__(master)    #Inicializa el frame
-        self.master.title("WikiCar - Login")    #Titulo de la ventana
-        self.master.resizable(width=False, height=False)    #Permite o no que la ventana cambie de tamano
-        self.master.geometry("900x600") #Configura el tamano de la ventana
-        self.master.config(bg='#17202A')  #Color de fondo
-        self.config(width=400, height=600, bg="#1B2631")
+        super().__init__(master)
+        self.master.config(bg="#17202A")
+        self.master.geometry("900x600")
         self.pack()
-        self.createWidget()
+        self.create_widgets()
+
+    def create_widgets(self):
+        mainFrame= Frame(self)
+        mainFrame.config(bg="#1B2631")
+        mainFrame.pack()
         
-    def createWidget(self):
-        self.welcomeLabel= Label(self, text="Welcome to WikiCar", fg="#ECF0F1", font='Arial 20 bold')
-        self.welcomeLabel.place(y=40)
-        self.welcomeLabel.config(bg="#1B2631")
+        welcomeLabel = Label(mainFrame, text="Welcome to WikiCar", fg="#ECF0F1", font=("Helveltic",20, BOLD))
+        welcomeLabel.config(bg="#1B2631", height=4)
+        welcomeLabel.pack()     
+        
+        loginLabel = Label(mainFrame, text="Login", fg="#ECF0F1", font=("Helvetic", 22, BOLD))
+        loginLabel.config(bg="#1B2631", height=2)
+        loginLabel.pack()
+        
+        centerFrame = Frame(self)
+        centerFrame.config(width=400, height=600, bg="#1B2631")
+        centerFrame.pack()
+                
+        
